a038af5db858e66037bde71dc34513d374c8e53f
Ritesh Pandit <134190903+R17358@users.noreply.github.com>
2025-01-20 15:51:15 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,7 @@ mkdir -p /opt/tesseract
 cd /opt/tesseract
 
 # Download prebuilt Tesseract binary
-wget https://github.com/tesseract-ocr/tesseract/releases/download/5.3.3/tesseract-5.3.3-linux-x86_64.tar.gz
+wget https://github.com/tesseract-ocr/tesseract/releases/tesseract-5.5.0.tar.gz
 
 # Extract it
 tar -xvzf tesseract-5.3.3-linux-x86_64.tar.gz
