cde6060256fad682d5e0a8de336ec366fb341114
Ritesh Pandit <134190903+R17358@users.noreply.github.com>
2025-01-20 22:45:58 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Create a directory for Tesseract in a writable location
-mkdir -p /opt/tesseract
-cd /opt/tesseract
+# Update package lists
+apt-get update
 
-# Download precompiled Tesseract binary (v5.5.0)
-wget https://github.com/UB-Mannheim/tesseract/wiki
+# Install Tesseract OCR
+apt-get install -y tesseract-ocr
 
-# Extract the Tesseract binary from the tar.gz file
-tar -xvzf tesseract-5.5.0-linux-x86_64.tar.gz
-
-# Move Tesseract binary to a directory in PATH
-mv tesseract-5.5.0/bin/tesseract /usr/local/bin/
-
-# Verify the installation
-/usr/local/bin/tesseract -v
+# Verify installation
+tesseract -v
